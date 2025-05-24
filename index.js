b2c41d0429eef9d4f8d8d469f053b0730079ddb6
augustotdz <augustotdz@users.noreply.github.com>
2025-05-24 18:36:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const knex = require('./db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cors = require('cors'); 
 
 const app = express();
 app.use(express.json());
